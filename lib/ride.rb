3e963a62883158ea4bfd3346e14ce57f9c04a15b
Katya Weicht <katya.weicht@gmail.com>
2025-03-10 09:11:28 -0600

    Write initialize method for ride class

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,4 +1,8 @@
 class Ride
+  attr_reader :name, :distance, :terrain
   def initialize(attributes)
+    @name = attributes[:name]
+    @distance = attributes[:distance]
+    @terrain = attributes[:terrain]
   end
 end
