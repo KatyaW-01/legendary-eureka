1f00fe058daea992e9266c3cf892632ec3405563
Katya Weicht <katya.weicht@gmail.com>
2025-03-10 09:39:00 -0600

    Write test for logging rides

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -29,9 +29,23 @@ RSpec.describe Biker do
       @biker.learn_terrain(:hills)
 
       expect(@biker.acceptable_terrain).to eq([:gravel, :hills])
-
     end
+    it 'can log rides' do
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
 
+      @biker.log_ride(ride1, 92.5)
+      @biker.log_ride(ride1, 91.1)
+      @biker.log_ride(ride2, 60.9)
+      @biker.log_ride(ride2, 61.6)
+
+      expected = {
+        ride1 => [92.5, 91.1],
+        ride2 => [60.9, 61.6]
+      }
+      
+      expect(@biker.rides).to eq(expected)
+    end
   end
 
 end
