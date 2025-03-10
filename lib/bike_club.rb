a6ea6f566d81770408b763315c9757a51233b899
Katya Weicht <katya.weicht@gmail.com>
2025-03-10 10:36:11 -0600

    write method and test for adding bikers

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -1,6 +1,11 @@
 class BikeClub
-  attr_reader :name
+  attr_reader :name, :bikers
   def initialize(name)
     @name = name
+    @bikers = []
+  end
+
+  def add_biker(biker)
+    @bikers << biker
   end
 end
