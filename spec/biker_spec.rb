d89f7bff10b215acb9e1d3d3233f77b4079d21a7
Katya Weicht <katya.weicht@gmail.com>
2025-03-10 09:26:28 -0600

    Write tests for name and max distance

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -10,6 +10,12 @@ RSpec.describe Biker do
     it 'exists' do
       expect(@biker).to be_a(Biker)
     end
+    it 'has a name' do
+      expect(@biker.name).to eq("Kenny")
+    end
+    it 'has a maximum distance' do
+      expect(@biker.max_distance).to eq(30)
+    end
   end
 
 end
