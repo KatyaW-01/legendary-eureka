45e4e7e8b13d2ea62440e09e3b0d6035ffde4db6
Katya Weicht <katya.weicht@gmail.com>
2025-03-10 11:20:20 -0600

    Write helper method and method for most_rides

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -8,4 +8,25 @@ class BikeClub
   def add_biker(biker)
     @bikers << biker
   end
+
+  def most_rides_helper
+    results = {}
+    @bikers.each do |biker|
+      if results[biker].nil?
+        results[biker] = 0
+      end
+      biker.rides.each do |ride,times|
+        results[biker] += times.length
+      end
+    end
+    results
+  end
+
+  def most_rides
+    superior_biker = most_rides_helper.max_by do |biker,rides|
+      rides
+    end
+    superior_biker[0]
+  end
+
 end
