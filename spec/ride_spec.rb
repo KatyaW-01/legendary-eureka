b5e3ac5d1e42e9a36fc1a32c7be70d32565c2d70
Katya Weicht <katya.weicht@gmail.com>
2025-03-10 11:44:26 -0600

    Add before each to ride spec

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -2,38 +2,29 @@ require './lib/ride'
 require 'pry'
 
 RSpec.describe Ride do
+  before(:each) do
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+  end
   describe "#initialize" do
     it 'exists' do
-      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
-
-      expect(ride1).to be_a(Ride)
+      expect(@ride1).to be_a(Ride)
     end
     it 'has a name' do
-      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
-
-      expect(ride1.name).to eq("Walnut Creek Trail")
+      expect(@ride1.name).to eq("Walnut Creek Trail")
     end
     it 'has a distance' do
-      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
-
-      expect(ride1.distance).to eq(10.7)
+      expect(@ride1.distance).to eq(10.7)
     end
     it 'has terrain' do
-      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
-
-      expect(ride1.terrain).to eq(:hills)
+      expect(@ride1.terrain).to eq(:hills)
     end
   end
   describe "#methods" do
     it 'returns true/false if a loop' do
-      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
-
-      expect(ride1.loop?).to eq(false)
+      expect(@ride1.loop?).to eq(false)
     end
     it 'has a total distance' do
-      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
-
-      expect(ride1.total_distance).to eq(21.4)
+      expect(@ride1.total_distance).to eq(21.4)
 
       ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
 
